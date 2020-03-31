88bec06ff88c3f1585775bd0a722184ec9bcbf0b
inesbaby <inse52086@gmail.com>
2020-03-31 21:54:49 +0800

    GroupBuy preparation

---
+++ vip/Controller/GroupBuyInformationController.swift
@@ -0,0 +1,21 @@
+//
+//  GroupBuyInformationController.swift
+//  vip
+//
+//  Created by Ines on 2020/3/31.
+//  Copyright © 2020 Ines. All rights reserved.
+//
+
+import UIKit
+
+class GroupBuyInformationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
