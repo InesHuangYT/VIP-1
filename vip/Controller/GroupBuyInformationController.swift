eba03581552daa654c92b85aa1b55a5162cb4a8b
inesbaby <inse52086@gmail.com>
2020-04-02 03:11:59 +0800

    group buy product information (continuing)

--- vip/Controller/GroupBuyInformationController.swift
+++ vip/Controller/GroupBuyInformationController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class GroupBuyInformationController: UIViewController {
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var productDescription: UILabel!
+    
+    @IBOutlet weak var productEvaluation: UILabel!
+    @IBOutlet weak var sellerEvaluation: UILabel!
+    
+    @IBOutlet weak var groupBuyImage: UIImageView!
     
     var index  = Int()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("index ",index)
        
     }
     
