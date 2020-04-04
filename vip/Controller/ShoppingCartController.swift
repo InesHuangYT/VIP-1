6bd9b13adac8d95fcdfdef74bc2c093fc340297a
rrou <qq649492@gmail.com>
2020-04-04 21:38:07 +0800

    Adjusting UI

--- vip/Controller/ShoppingCartController.swift
+++ vip/Controller/ShoppingCartController.swift
@@ -12,27 +12,46 @@ import Firebase
 
 
 struct CellData{
+    let id : String?
     let image : String?
     let productName : String?
     let price : String?
 }
 
 class ShoppingCartController : UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    var data = [CellData]()
+    var ref: DatabaseReference!
+    var Data = [CellData]()
+    var test:UILabel!
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         tableView.delegate = self
         tableView.dataSource = self
-        
+        loadData()
         super.viewDidLoad()
-        data = [CellData.init(image: "image", productName: "name", price: "$150")]
+//        data = [CellData.init(image: "image", productName: "name", price: "$150")]
     }
    
     func loadData(){
-        
+        Database.database().reference().child("ShoppingCart")
+                  .observeSingleEvent(of: .value, with: { snapshot in
+                    print(snapshot.children.allObjects)
+//                    print("SNAP key:",snapshot.)
+                    if let data = snapshot.children.allObjects as? [DataSnapshot] {
+//                        let retriId = data.
+                        let retriName = data.compactMap({($0.value as! [String:Any])["ProductName"]})
+                        print("retriName:",retriName)
+                        
+                        
+                    }
+                    
+                    
+          
+         
+
+          
+          })
     }
     
     func numberOfSectionInTableView(tableView: UITableView) -> Int{
