c88b9e0eddb39930d3d43c092dad887303df9f10
rrou <qq649492@gmail.com>
2020-04-05 17:16:11 +0800

    剩下指定cevllnumber

--- vip/Controller/ShoppingCartController.swift
+++ vip/Controller/ShoppingCartController.swift
@@ -16,59 +16,58 @@ import Firebase
 class ShoppingCartController : UIViewController, UITableViewDelegate, UITableViewDataSource {
     var ref: DatabaseReference!
     var test:UILabel!
-    var numCell:Int!
-    var name:[String] = []
+    var count = Int()
+    var name:[Any] = []
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadData()
-        tableView.delegate = self
-        tableView.dataSource = self
-        print("lookup name = ", self.name)
-        //        data = [CellData.init(image: "image", productName: "name", price: "$150")]
-    }
-    
-    func loadData(){
+        
         ref = Database.database().reference()
         let user = Auth.auth().currentUser!
-        let queue = DispatchQueue(label: "com.appcoda.myqueue")
-        queue.sync {
-            self.ref.child("ShoppingCart").child(user.uid).observeSingleEvent(of: .value, with: { snapshot in
-                if let data = snapshot.children.allObjects as? [DataSnapshot] {
-                    print(data)
-                    let retriName = data.compactMap({($0.value as! [String:Any])["ProductName"]})
-                    print("retriName:",retriName)
-                    let retriprice = data.compactMap({($0.value as![String:Any])["Price"]})
-                    let imageURL = data.compactMap({
-                        ($0.value as! [String: Any])["imageURL"]
-                    })
-                    self.name = retriName as! [String]
-                    
-                }
-                
-            })
-        }
         
+        DispatchQueue.main.async {
+        self.ref.child("ShoppingCart").child(user.uid).observeSingleEvent(of: .value, with: { snapshot in
+                if let data = snapshot.children.allObjects as? [DataSnapshot] {
+                let retriName = data.compactMap({($0.value as! [String:Any])["ProductName"]})
+                self.name = retriName
+                    
+            }
+            
+        })
+            self.tableView.reloadData()
+        }
+        print("num ITEM = ", self.name.count)
+        
+//        print("ViewCount2 self.count= ", self.count)
+//        print("ViewCount2 = ", count)
+//
+        
+        //        self.tableView.delegate = self
+        //        self.tableView.dataSource = self
         
     }
+    
+    
+    
+   
+   
     
     func numberOfSectionInTableView(tableView: UITableView) -> Int{
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
-        return 3
+               
+        return name.count
     }
     
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath) as! ShoppingCartCell
-        cell.ProductName.text = ""
-        cell.Price.text = ""
-        
+        cell.loadData(index: indexPath.row)
         
         return cell
     }
